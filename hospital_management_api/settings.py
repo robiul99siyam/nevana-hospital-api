c81abf959373aca84ee5faf93636d2689a7089f9
robiul99siyam <siyamrobiulislam@gmail.com>
2024-01-30 12:38:01 +0600

    Allowed host

--- hospital_management_api/settings.py
+++ hospital_management_api/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure--idz04x*hub4ub9x=78w@lcbq9+yq)ops&tb_qx9i^q^7ligha
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://nevana-api.onrender.com','https://*.127.0.0.1']
+
 
 
 # Application definition
