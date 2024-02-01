41739ea9355f3f9cf0ce460b4223ecccba0bd74f
robiul99siyam <siyamrobiulislam@gmail.com>
2024-02-01 11:02:33 +0600

    api project renew

--- doctor/views.py
+++ doctor/views.py
@@ -30,7 +30,7 @@ class ReviewerViewset(viewsets.ModelViewSet):
 
 
 class DoctorPagination(pagination.PageNumberPagination):
-    page_size = 6
+    page_size = 4
     page_size_query_param = page_size
     max_page_size = 100
 class DoctorViewset(viewsets.ModelViewSet):
